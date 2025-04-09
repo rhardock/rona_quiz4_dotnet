5e26daa93c53a59dd740bcc651a068c06edd961c
Ronald Hardock <rhardock@my.centennialcollege.ca>
2025-04-09 12:17:00 -0400

    Added Database access app

---
+++ DatabaseAccessApp/DatabaseAccessApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseAccessApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Database access has already been done on April 9, 2025 by Ronald Hardock");
+        }
+    }
+}
